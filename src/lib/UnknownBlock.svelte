570959502f286f9b46ed2d7bcae1ba7a09e73780
Michael Aufreiter <michael@letsken.com>
2024-10-14 20:41:36 +0200

    Render a custom container cursor.

--- src/lib/UnknownBlock.svelte
+++ src/lib/UnknownBlock.svelte
@@ -1,24 +1,10 @@
 <script>
-  import { getContext } from 'svelte';
-  const svedit = getContext('svedit');
-
   let {
     path,
     block
   } = $props();
-
-  function is_selected() {
-    return false;
-  }
-
 </script>
 
-<div data-path={path.join('.')} data-type="block" class:selected={is_selected()}>
+<div data-path={path.join('.')} data-type="block">
   Unknown block type {block.type}
 </div>
-
-<style>
-  .selected {
-    outline: 1px solid #0075d5;
-  }
-</style>
