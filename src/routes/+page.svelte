72f745b8ddc8abd5bc4fd894cdd6e13e90e22fea
Michael Aufreiter <michael@letsken.com>
2024-10-16 15:29:10 +0200

    Fix the demo contents.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,27 +13,27 @@
       [24, 44, 'emphasis']
     ]],
     body: [
-      { type: 'story', layout: 1, image: '/images/undraw_happy_feeling_re_e76r.svg', title: ['Visual in-place editing', []], description: ['Model your content in JSON, render it with Svelte components, and have text editable directly in the layout. Extend with custom components to render Vimeo Embeds, Forms, Tables, & more.', []] },
-      { type: 'story', layout: 2, image: '/images/lightweight.svg', title: ['Lightweight', []], description: ['The reference implementation uses only about 1000 lines of code. That means you\'ll be able to serve editable web pages.', []] },
+      { type: 'story', layout: 1, image: '/images/undraw_happy_feeling_re_e76r.svg', title: ['Visual in-place editing', []], description: ['Model your content in JSON, render it with Svelte components, and edit content directly in the layout. You only have to follow a couple of rules to make this work.', []] },
+      { type: 'story', layout: 2, image: '/images/lightweight.svg', title: ['Lightweight', []], description: ["The reference implementation uses only about 1000 lines of code. That means you'll be able to serve editable web pages, removing the need for a separate Content Management System.", [[100,118, "link", { "href": "https://editable.website"}]]] },
       { type: 'story', layout: 1, image: '/images/nested-blocks-illustration.svg', title: ['Nested blocks', []], description: ['A block can embed a container of other blocks. For instance the list block below has a container of list items.', []] },
-      { type: 'story', layout: 2, image: '/images/container-cursors.svg', title: ['Container cursors', []], description: ['They work just like text cursors, but instead of a character position in a string they address a block position in a container.', []] },
+      { type: 'story', layout: 2, image: '/images/container-cursors.svg', title: ['Container cursors', []], description: ['They work just like text cursors, but instead of a character position in a string they address a block position in a container.\n\nTry it by selecting a few blocks, then press ↑ or ↓. Press ↵ to insert a new block or ⌫ to delete the block before the cursor.', []] },
       // -- 
       { type: 'story', layout: 1, image: '/images/svelte-logo.svg', title: ['Made for Svelte 5', []], description: ['Integrate with your Svelte application. Use it as a template and copy and paste Svedit.svelte to build your custom rich content editor.', [ [20, 26, "link", {"href": "https://svelte.dev/"}], [80, 93, "emphasis", null] ]] },
-      { type: 'story', layout: 2, image: '/images/undraw_undraw_undraw_undraw_undraw_undraw_undraw_undraw_users_per_minute_1e4q_t22j_-1-_0ngf_-1-_27dv_30ul_legv_-1-_il1l_-2-_0jip.svg', title: ['Alpha version', []], description: ['Expect bugs. Expect missing features. Expect the need for more work on your part to make this work for your use case.', []] },
+      { type: 'story', layout: 2, image: '/images/undraw_undraw_undraw_undraw_undraw_undraw_undraw_undraw_users_per_minute_1e4q_t22j_-1-_0ngf_-1-_27dv_30ul_legv_-1-_il1l_-2-_0jip.svg', title: ['Alpha version', []], description: ['Expect bugs. Expect missing features. Expect the need for more work on your part to make this work for your use case.\n\n Find below a list of known issues we\'ll be working to get fixed next:', []] },
       {
         type: 'list',
         list_style: 'decimal-leading-zero',
         items: [
-          { type: 'list_item', description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo viverra mi sit amet semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.', []] },
-          { type: 'list_item', description: ['Donec eget auctor nibh, vel porttitor mauris. Curabitur in feugiat ante. Mauris feugiat pellentesque quam, accumsan condimentum leo viverra nec. Nam scelerisque nisl at mauris rutrum sodales.', []] },
-          { type: 'list_item', description: ['Integer auctor finibus turpis non egestas. Nullam eleifend odio magna, id vehicula est efficitur id. Suspendisse sem massa, congue condimentum nulla ut, fermentum tincidunt lectus.', []] },
-          { type: 'list_item', description: ['Suspendisse pretium, neque et eleifend ultrices, leo mi tempor justo, et eleifend nisl nulla ac est.', []] },
-          { type: 'list_item', description: ['Donec eget auctor nibh, vel porttitor mauris. Curabitur in feugiat ante. Mauris feugiat pellentesque quam, accumsan condimentum leo viverra nec. Nam scelerisque nisl at mauris rutrum sodales.', []] },
+          { type: 'list_item', description: ['Images can not yet be selected and changed. We\'ll solve this by making any non-text property selectable on the canvas, and show a popover (e.g. an image selector, or a math formula editor) to make changes, which will then be reflected in the canvas display immediately.' , []] },
+          { type: 'list_item', description: ['Container selections inside nested blocks (e.g. list items in this list) do not work reliably yet.', []] },
         ]
       },
     ]
   });
 
+
+  
+        
 </script>
 
 <div class="demo-wrapper pbs-6">
