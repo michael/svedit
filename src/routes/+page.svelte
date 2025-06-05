40074e8b66729890c86fae5bbe79f16b16e99cb1
Michael Aufreiter <michael@letsken.com>
2025-06-05 21:29:25 +0200

    Use original demo document.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,14 +44,24 @@
         ref_types: ['list_item'],
         default_ref_type: 'list_item',
       },
+      list_style: { type: 'string' },
     },
   };
 
-  const story_1_id = svid();
+  // Generate IDs for all content nodes
   const page_1_id = svid();
+  const story_1_id = svid();
+  const story_2_id = svid();
+  const story_3_id = svid();
+  const story_4_id = svid();
+  const story_5_id = svid();
+  const story_6_id = svid();
+  const story_7_id = svid();
   const list_1_id = svid();
   const list_item_1_id = svid();
   const list_item_2_id = svid();
+  const list_item_3_id = svid();
+  const list_item_4_id = svid();
 
   const raw_doc = [
     {
@@ -59,29 +69,94 @@
       type: 'story',
       layout: 1,
       image: '/images/editable.svg',
-      title: ['Visual inplace editing', []],
+      title: ['Visual in‑place editing', []],
       description: ['Model your content in JSON, render it with Svelte components, and edit content directly in the layout. You only have to follow a couple of rules to make this work.', []]
+    },
+    {
+      id: story_2_id,
+      type: 'story',
+      layout: 2,
+      image: '/images/lightweight.svg',
+      title: ['Minimal viable editor', []],
+      description: ["The reference implementation uses only about 1000 lines of code. That means you'll be able to serve editable web pages, removing the need for a separate Content Management System.", [[100,118, "link", { "href": "https://editable.website"}]]]
+    },
+    {
+      id: story_3_id,
+      type: 'story',
+      layout: 1,
+      image: '/images/nested-blocks-illustration.svg',
+      title: ['Nested blocks', []],
+      description: ['A block can embed a container of other blocks. For instance the list block at the bottom of the page has a container of list items.', []]
+    },
+    {
+      id: story_4_id,
+      type: 'story',
+      layout: 2,
+      image: '/images/container-cursors.svg',
+      title: ['Container cursors', []],
+      description: ['They work just like text cursors, but instead of a character position in a string they address a block position in a container.\n\nTry it by selecting a few blocks, then press ↑ or ↓. Press ↵ to insert a new block or ⌫ to delete the block before the cursor.', []]
+    },
+    {
+      id: story_5_id,
+      type: 'story',
+      layout: 1,
+      image: '/images/svelte-logo.svg',
+      title: ['Made for Svelte 5', []],
+      description: ['Integrate with your Svelte application. Use it as a template and copy and paste Svedit.svelte to build your custom rich content editor.', [ [20, 26, "link", {"href": "https://svelte.dev/"}], [80, 93, "emphasis", null] ]]
+    },
+    {
+      id: story_6_id,
+      type: 'story',
+      layout: 2,
+      image: '/images/extendable.svg',
+      title: ['Alpha version', []],
+      description: ['Expect bugs. Expect missing features. Expect the need for more work on your part to make this work for your use case.\n\nFind below a list of known issues we\'ll be working to get fixed next:', []]
     },
     {
       id: list_item_1_id,
       type: 'list_item',
-      content: ['first list item', []],
+      content: ['Images can not yet be selected and changed. We\'ll solve this by making any non‑text property selectable on the canvas, and show a popover (e.g. an image selector, or a math formula editor) to make changes, which will then be reflected in the canvas display immediately.', []]
     },
     {
       id: list_item_2_id,
       type: 'list_item',
-      content: ['second list item', []],
+      content: ['Container selections inside nested blocks (e.g. list items in this list) do not work reliably yet.', []]
+    },
+    {
+      id: list_item_3_id,
+      type: 'list_item',
+      content: ['Only the latest Chrome is supported at the moment as we rely on CSS Anchor Positioning for overlays.', []]
+    },
+    {
+      id: list_item_4_id,
+      type: 'list_item',
+      content: ['Full mobile support is considered in our design, but not yet implemented.', []]
     },
     {
       id: list_1_id,
       type: 'list',
-      list_items: [list_item_1_id, list_item_2_id],
+      list_items: [list_item_1_id, list_item_2_id, list_item_3_id, list_item_4_id],
+      list_style: 'decimal-leading-zero',
+    },
+    {
+      id: story_7_id,
+      type: 'story',
+      layout: 1,
+      image: '/images/github.svg',
+      title: ['Star us on GitHub', []],
+      description: ['Please star Svedit on GitHub or watch the repo to be notified about updates. Svedit is made by Michael Aufreiter and Johannes Mutter and is licensed under the MIT License.', 
+        [
+          [0, 28, "link", {"href": "https://github.com/michael/svedit/", target: "_blank"}],
+          [95, 112, "link", {"href": "https://michaelaufreiter.com", target: "_blank"}],
+          [117,132, "link", {"href": "https://mutter.co", target: "_blank"}],
+        ]
+      ]
     },
     // IMPORTANT: The root node (entry point) must be the last one in the array
     {
       id: page_1_id,
       type: 'page',
-      body: [story_1_id, story_1_id, list_1_id],
+      body: [story_1_id, story_2_id, story_3_id, story_4_id, story_5_id, story_6_id, list_1_id, story_7_id],
       keywords: ['svelte', 'editor', 'rich content'],
       daily_visitors: [10, 20, 30, 100],
       created_at: '2025-05-30T10:39:59.987Z'
