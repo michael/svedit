662f9d0fa8eb09e626e824c87fa5a263f95cc1e3
Michael Aufreiter <michael@letsken.com>
2025-08-11 15:47:29 +0200

    Upgrade libs. Get rid of playwright and jsdom dependencies.

--- e2e/demo.test.js
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('home page has .svedit element', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('.svedit')).toBeVisible();
-});
