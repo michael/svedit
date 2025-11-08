b86e6aaf1f8b29a84641960f4407150ae3036489
Michael Aufreiter <michael@letsken.com>
2025-11-08 18:32:17 +0100

    Upgrade all dependencies.
    Upgrade to Vitest 4, which marks the first stable release of
    browsermode.

--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vitest/config';
+import { preview } from '@vitest/browser-preview';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		browser: {
-			// NOTE: We don't need playwright, we just open the real Chrome
-			// provider: 'playwright', // or 'webdriverio'
+			provider: preview(),
 			enabled: true,
 			// at least one instance is required
 			instances: [{ browser: 'chromium' }]
