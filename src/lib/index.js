3539a8341f0549652731e5d6f27f6d25f0ac9302
Michael Aufreiter <michael@letsken.com>
2025-11-11 16:31:07 +0100

    Introduce KeyMapper API for mapping keyboard shortcuts to commands.
    Multiple keymaps and command scopes can be used and registered via a
    scope stack.

--- src/lib/index.js
+++ src/lib/index.js
@@ -9,6 +9,9 @@ export { default as NodeArrayProperty } from './NodeArrayProperty.svelte';
 export { default as Document, define_document_schema } from './Document.svelte.js';
 export { default as Transaction } from './Transaction.svelte.js';
 
+// Keyboard handling
+export { KeyMapper, define_keymap } from './KeyMapper.svelte.js';
+
 // Commands and utilities
 export * from './commands.svelte.js';
 export * from './util.js';
