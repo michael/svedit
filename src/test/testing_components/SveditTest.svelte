2f3ffe62a153059734a0cd28560832a3ef100a18
Michael Aufreiter <michael@letsken.com>
2025-11-25 13:40:53 +0100

    Fix tests.

--- src/test/testing_components/SveditTest.svelte
+++ src/test/testing_components/SveditTest.svelte
@@ -2,9 +2,9 @@
 	import Svedit from '../../lib/Svedit.svelte';
 	import Layout from '../../routes/components/Layout.svelte';
 
-	let { doc } = $props();
+	let { editor_state } = $props();
 </script>
 
 <Layout>
-	<Svedit {doc} editable={true} path={[doc.document_id]} />
+	<Svedit {editor_state} editable={true} path={[editor_state.document_id]} />
 </Layout>
