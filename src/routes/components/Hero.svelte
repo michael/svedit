31c1187737f1ae25287b6e99c5f7334df140544f
Michael Aufreiter <michael@letsken.com>
2026-04-29 01:15:45 +0200

    Improve debug output styles

--- src/routes/components/Hero.svelte
+++ src/routes/components/Hero.svelte
@@ -42,12 +42,11 @@
 
 <style>
 	:global(.hero) {
-		margin-bottom: var(--s-10);
 		padding-inline-start: max(var(--s-10), env(safe-area-inset-left, 0px));
 		padding-inline-end: max(var(--s-10), env(safe-area-inset-right, 0px));
 		padding-block-start: max(var(--s-10), env(safe-area-inset-top, 0px));
 		padding-block-end: max(var(--s-10), env(safe-area-inset-bottom, 0px));
-		padding: var(--s-10) var(--s-6);
+		padding: 100px var(--s-6);
 		display: flex;
 		align-items: center;
 		justify-content: center;
